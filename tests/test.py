560d73a4a9f9dcf5da66e79a939148a90d7982f7
Sarah Monestier <sarah.monestier@ens-lyon.fr>
2026-01-29 14:06:31 +0100

    Good tests !!

--- tests/test.py
+++ tests/test.py
@@ -1,23 +1,35 @@
 from colorprint import colorprint as cprint 
 import subprocess
+import os
+
+overhead = "let prInt x = print_int x;print_newline(); x;;"
 
 def test(file):
-    with open(f"{file.name}", "r") as f:
-        result = subprocess.run(
+    with open(f"tests/{file}", "r") as f:
+        with open("tests/temp.ml", "w") as tf:
+            tf.write(overhead+f.read())
+    with open("tests/temp.ml", "r") as f:
+        caml_result = subprocess.run(
+            ["ocaml", "tests/temp.ml"],
+            capture_output=True,
+            text=True,
+            check=True
+        )
+    with open(f"tests/{file}", "r") as f:
+        fouine_result = subprocess.run(
             ["./_build/default/bin/fouine.exe"],
             stdin=f,
             capture_output=True,
             text=True,
             check=True
         )
-    (output, error) = (result.stdout, result.stderr)
-    if error != file.expected_error:
-        cprint(f"Error not conform in {file.name}, espected {file.expected_error}, got {error}", "red")
-    if output != file.expected_output:
-        cprint(f"Output not conform in {file.name}, espected {file.expected_output}, got {output}", "red")
-    if output == file.expected_output and error == file.expected_error:
-        cprint(f"Test {file.name} passed", "green")
-    return (output, error)
+    # TODO : better handle when error (relaunch fouine in debug mode ?)
+    if fouine_result.stderr != caml_result.stderr:
+        cprint(f"Error not conform in {file}, espected {caml_result.stderr}, got {fouine_result.stderr}", "red")
+    if fouine_result.stdout != caml_result.stdout:
+        cprint(f"Output not conform in {file}, espected {caml_result.stdout}, got {fouine_result.stdout}", "red")
+    if fouine_result.stdout == caml_result.stdout and fouine_result.stderr == caml_result.stderr:
+        cprint(f"Test {file} passed", "green")
 
 class File:
     def __init__(self, name, expected_output=b'', expected_error=b''):
@@ -25,12 +37,10 @@ class File:
         self.expected_output  = expected_output
         self.expected_error   = expected_error
 
-# TODO : compare ocaml output and normal output
-# TODO : run fouine in debug mode on error
-files_to_test = [
-    File("if_true", "1\n", ""),
-    File("if_false", "0\n", ""),
+ml_files = [
+    f for f in os.listdir("tests")
+    if f.endswith(".ml") and f != "temp.ml"
 ]
 
-for file in files_to_test:
+for file in ml_files:
     test(file)
