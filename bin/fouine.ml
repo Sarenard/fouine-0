a6d2d7994d6826bbdfec5da2f9a9b8a374fd2c9b
Sarah Monestier <sarah.monestier@ens-lyon.fr>
2026-02-12 14:04:14 +0100

    Typage part 1 (standalone)

--- bin/fouine.ml
+++ bin/fouine.ml
@@ -2,22 +2,30 @@ open Lib
 
 let nom_fichier = ref ""
 let debug = ref false
+(*this is temporary*)
+let typage = ref false
 
 let recupere_entree () =
   Arg.parse (* ci-dessous les 3 arguments de Arg.parse : *)
     [
       ("-d", Arg.Set debug, "Mode debug");
+      ("-t", Arg.Set typage, "Test du typage");
     ] (* la liste des options, vide *)
     (fun s -> nom_fichier := s) (* la fonction a declencher lorsqu'on recupere un string qui n'est pas une option : ici c'est le nom du fichier, et on stocke cette information dans la reference nom_fichier *)
     ""; (* le message d'accueil, qui est vide *)
+  if !typage then
+    (Typing.main ())
+  else
   try
     let where_from = match !nom_fichier with
       | "" -> stdin
       | s -> open_in s in
     let lexbuf = Lexing.from_channel where_from in
     let parse () = Parser.main Lexer.token lexbuf in
-    parse () 
-  with e -> (Printf.printf "Asteriiiiiiiiiiiiiiix\n"; raise e)
+    parse ()
+  with e -> (
+    Printf.printf "Asteriiiiiiiiiiiiiiix\n"; raise e
+  )
 
 let run () =
   try
